ddcde50b010df89274b56bb610338049e8da4de2
Michael Ekstrand <md@ekstrandom.net>
2024-05-31 14:56:42 -0400

    try aggregate code, hit bug

--- movielens/aggregate-ml.py
+++ movielens/aggregate-ml.py
@@ -17,6 +17,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from docopt import docopt
+from duckdb import DuckDBPyConnection, connect
 from sandal import autoroot  # noqa: F401
 from sandal.cli import setup_logging
 
@@ -24,33 +25,32 @@ _log = logging.getLogger("codex.aggregate-ml")
 
 
 def main(options):
-    zipf = Path(options["ZIPFILE"])
-    _log.info("importing %s", zipf)
+    sets = options["DS"]
+    _log.info("summarizing %d data sets", len(sets))
+    if os.path.exists("ml-stats.duckdb"):
+        _log.info("removing ml-stats.duckdb")
+        os.unlink("ml-stats.duckdb")
 
-    data = open_data(zipf)
-
-    pqf = zipf.with_name("ratings.parquet")
-
-    with data:
-        dirname = data.get_dirname()
-        with data.archive.open(os.path.join(dirname, data.rating_file)) as rf:
-            rate_df = data.read_ratings(rf)
-        _log.info("read %d ratings", len(rate_df))
-
-        rate_df["timestamp"] = pd.to_datetime(rate_df["timestamp"], unit="s")
-
-        _log.info("saving %s", pqf)
-        rate_df.to_parquet(pqf, index=False, compression="zstd")
+    with connect("ml-stats.duckdb") as db:
+        initialize_db(db, sets)
+        union_tables(db, "global_stats", sets)
 
 
-def open_data(file: Path) -> MLData:
-    name = file.stem
-    if name == "ml-100k":
-        return ML100K(file)
-    elif name == "ml-1m" or name == "ml-10m":
-        return MLM(file)
-    else:
-        return MLCurrent(file)
+def initialize_db(db: DuckDBPyConnection, sets: list[str]):
+    set_names = ", ".join(f"'{n}'" for n in sets)
+    db.execute(f"CREATE TYPE ml_set AS ENUM({set_names})")
+    for name in sets:
+        _log.info("attaching %s", name)
+        db.execute(f"ATTACH '{name}/ratings.duckdb' AS {name} (READONLY)")
+
+
+def union_tables(db: DuckDBPyConnection, table: str, sets: list[str]):
+    _log.info("aggregating table %s", table)
+    query = f"CREATE TABLE {table} AS "
+    selects = [f"SELECT '{n}', * FROM {n}.{table}" for n in sets]
+    query += " UNION ALL ".join(selects)
+    _log.debug("query: %s", query)
+    db.execute(query)
 
 
 if __name__ == "__main__":
