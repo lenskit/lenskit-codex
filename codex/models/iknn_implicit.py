2e2ea4c77c426c42dfde074b00a174821700b9b5
Michael Ekstrand <mdekstrand@drexel.edu>
2024-07-17 20:43:00 +0000

    iknn

--- codex/models/iknn_implicit.py
+++ codex/models/iknn_implicit.py
@@ -10,3 +10,11 @@ sweep_space = {
     "nnbrs": [5, 10, 15, 25, 35, 50],
     "min_nbrs": [1, 2],
 }
+
+
+def default():
+    return ItemItem(20, feedback="implicit")
+
+
+def from_config(nnbrs, min_nbrs):
+    return ItemItem(nnbrs, min_nbrs=min_nbrs, feedback="implicit")
