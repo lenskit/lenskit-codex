9bb80be390171eea2757911861943b5064bf046e
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-28 22:46:03 +0000

    add qrel export

--- amazon/2023-5core/pipe-scan.ts
+++ amazon/2023-5core/pipe-scan.ts
@@ -1,3 +1,4 @@
+import { mapNotNullish } from "std/collections/mod.ts";
 import { action_cmd } from "../../codex/dvc.ts";
 
 import { sourceFiles } from "./pipe-sources.ts";
@@ -44,6 +45,22 @@ export const scanStages = {
       ],
       outs: [
         "data/${item}.parquet",
+      ],
+    },
+  },
+  "export-qrels": {
+    foreach: mapNotNullish(sourceFiles, (s) => s.part == "test" ? s.name : null),
+    do: {
+      cmd: action_cmd(
+        import.meta.url,
+        "trec export qrels",
+        "data/${item}.parquet",
+      ),
+      deps: [
+        "data/${item}.parquet",
+      ],
+      outs: [
+        "data/${item}.qrels.gz",
       ],
     },
   },
