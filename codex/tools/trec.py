f0082c95897bb9e0cd1de555a439920a74abbe3e
Michael Ekstrand <mdekstrand@drexel.edu>
2024-08-29 16:58:46 +0000

    add run export

--- codex/tools/trec.py
+++ codex/tools/trec.py
@@ -3,7 +3,7 @@ from os import fspath
 from pathlib import Path
 
 import click
-import duckdb
+from duckdb import CoalesceOperator, ColumnExpression, ConstantExpression, connect
 
 from . import codex
 
@@ -25,11 +25,12 @@ def export():
 @export.command("qrels")
 @click.argument("FILE", type=Path)
 def export_qrels(file: Path):
+    "Export QREL files from truth data."
     if file.suffix != ".parquet":
         _log.info("only parquet files can be exported at present")
 
     outf = file.with_suffix(".qrels.gz")
-    with duckdb.connect() as db:
+    with connect() as db:
         _log.info("reading test data from %s", file)
         tbl = db.read_parquet(fspath(file))
         qrels = tbl.query(
@@ -42,3 +43,28 @@ def export_qrels(file: Path):
         )
         _log.info("saving qrels to %s", outf)
         qrels.write_csv(fspath(outf), sep="\t", header=False, compression="gzip")
+
+
+@export.command("runs")
+@click.option("-r", "--run", type=int, help="select a single run to output")
+@click.argument("DB", type=Path)
+@click.argument("OUT", type=Path)
+def export_runs(db: Path, out: Path, run: int | None = None):
+    "Export runs in TREC-compatible format."
+    # <query id><iteration><document id><rank><score>[<run id>]
+
+    with connect(fspath(db), read_only=True) as cxn:
+        rel = cxn.table("recommendations")
+        if run is not None:
+            rel = rel.filter(ColumnExpression("run") == ConstantExpression(run))
+        rel = rel.order("run, user, rank")
+        rel = rel.select(
+            "user",
+            ConstantExpression(0),
+            "item",
+            "rank",
+            CoalesceOperator(ColumnExpression("score"), ConstantExpression(0)),
+            "run",
+        )
+        _log.info("saving run to %s", out)
+        rel.write_csv(fspath(out), sep="\t", header=False, compression="gzip")
