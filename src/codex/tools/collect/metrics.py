65b8549e1d6b3726db0d7baa972d9481d351c837
Michael Ekstrand <mdekstrand@drexel.edu>
2024-12-28 22:50:06 -0500

    working metric collection script

--- src/codex/tools/collect/metrics.py
+++ src/codex/tools/collect/metrics.py
@@ -1,118 +1,85 @@
-import logging
 from collections.abc import Generator
-from os import fspath
 from pathlib import Path
 
 import click
-from duckdb import connect
+import pandas as pd
+import structlog
+
+from codex.runlog import CodexTask
 
 from . import collect
 
-_log = logging.getLogger(__name__)
-
-METRIC_AGG_DDL = """
-DROP TABLE IF EXISTS run_files;
-DROP SEQUENCE IF EXISTS run_fileno;
-CREATE SEQUENCE run_fileno START 1;
-CREATE TABLE run_files (
-    fileno INT PRIMARY KEY DEFAULT nextval('run_fileno'),
-    filename VARCHAR NOT NULL UNIQUE,
-);
-DROP TABLE IF EXISTS run_metrics;
-CREATE TABLE run_metrics (
-    fileno INT NOT NULL,
-    run TINYINT NOT NULL,
-    ndcg FLOAT,
-    mrr FLOAT,
-    user_rmse FLOAT
-);
-DROP TABLE IF EXISTS user_metrics;
-CREATE TABLE user_metrics (
-    fileno INT NOT NULL,
-    run TINYINT NOT NULL,
-    wall_time FLOAT,
-    nrecs INT,
-    ntruth INT,
-    ndcg FLOAT,
-    recip_rank FLOAT,
-    rmse FLOAT,
-    mae FLOAT,
-);
-"""
+_log = structlog.stdlib.get_logger(__name__)
+_run_base = Path("runs")
 
 
 @collect.command("metrics")
-@click.option("--view-script", type=Path, help="SQL script to create derived views")
-@click.option("-g", "--glob", help="Glob to select files from directories.")
-@click.argument("DBFILE", type=Path)
-@click.argument("INPUTS", type=Path, nargs=-1, required=True)
-def collect_metrics(
-    dbfile: Path, inputs: list[Path], view_script: Path | None = None, glob: str | None = None
-):
-    "Collect metrics from runs across DB files."
-    _log.info("opening output database %s", dbfile)
-    with connect(fspath(dbfile)) as db:
-        db.execute(METRIC_AGG_DDL)
-        for src_db in find_inputs(inputs, glob):
-            _log.info("reading %s", src_db)
-            db.execute(f"ATTACH '{src_db}' AS src (READ_ONLY)")
+@click.option("-S", "--summary-output", metavar="FILE", type=Path, help="Summary output file.")
+@click.option("-U", "--user-output", metavar="FILE", type=Path, help="User-level output file.")
+@click.option("-L", "--run-list", type=Path, metavar="FILE", help="Load run list from FILE.")
+def collect_metrics(summary_output: Path, user_output: Path, run_list: Path):
+    "Collect metrics from runs for a data set."
+    _log.debug("reading run list")
+    runs = pd.read_csv(run_list)
+    runs = runs.set_index("path").astype("category")
 
-            db.execute(
-                "INSERT INTO run_files (filename) VALUES (?) RETURNING fileno", [src_db.as_posix()]
-            )
-            fnrow = db.fetchone()
-            assert fnrow is not None
-            (fileno,) = fnrow
+    _log.info("collecting metrics from %d runs", len(runs))
+    user_metrics = {path: _collect_user_metrics(_run_base / path) for path in runs.index}
+    metric_df = pd.concat(user_metrics, names=["path"])
+    summaries = (
+        metric_df.drop(columns=["user_id"])
+        .astype({"part": "str"})
+        .groupby(["path", "part"])
+        .agg("mean")
+    )
 
-            db.execute("""
-                SELECT column_name FROM duckdb_columns()
-                WHERE database_name = 'src'
-                AND table_name = 'user_metrics'
-            """)
-            src_cols = [r[0] for r in db.fetchall()]
+    metric_df = runs.join(metric_df)
 
-            ucols = "ndcg, recip_rank"
-            cols = "ndcg, mrr"
-            agg = "AVG(ndcg) AS ndcg, AVG(recip_rank) AS mrr"
-            if "rmse" in src_cols:
-                ucols += ", rmse, mae"
-                cols += ", user_rmse"
-                agg += ", AVG(rmse) AS user_rmse"
+    _log.info("saving user-level metrics output", file=str(user_output))
+    metric_df.to_parquet(user_output, index=False, compression="zstd")
 
-            db.execute(
-                f"""
-                    INSERT INTO run_metrics (fileno, run, {cols})
-                    SELECT ?, run, {agg}
-                    FROM src.user_metrics
-                    GROUP BY run
-                """,
-                [fileno],
-            )
-
-            db.execute(
-                f"""
-                    INSERT INTO user_metrics (fileno, run, {ucols})
-                    SELECT ?, run, {ucols}
-                    FROM src.user_metrics
-                """,
-                [fileno],
-            )
-
-            _log.debug("detaching source database")
-            db.execute("DETACH src")
-
-        if view_script is not None:
-            _log.info("running %s", view_script)
-            sql = view_script.read_text()
-            db.execute(sql)
+    times = pd.concat(_collect_run_metrics(path) for path in runs.index)
+    summary_df = runs.join(times)
+    summary_df = summary_df.join(summaries)
+    summary_df = summary_df.reset_index("part")
+    _log.info("saving summary metrics output", file=str(summary_output))
+    summary_df.to_csv(summary_output, index=False)
 
 
-def find_inputs(inputs: list[Path], glob: str | None) -> Generator[Path]:
-    for src in inputs:
-        if src.is_dir():
-            if glob is None:
-                _log.warn("no glob specified, defaulting to **/*.duckdb")
-                glob = "**/*.duckdb"
-            yield from src.glob(glob)
-        else:
-            yield src
+def _collect_user_metrics(path: Path) -> pd.DataFrame:
+    _log.debug("reading user metrics", path=str(path))
+    return pd.read_json(path / "user-metrics.ndjson.zst", lines=True)
+
+
+def _collect_run_metrics(path: str) -> pd.DataFrame:
+    train_tasks = {task.data.part: task for task in _read_tasks(_run_base / path / "training.json")}
+    infer_tasks = {
+        task.data.part: task for task in _read_tasks(_run_base / path / "inference.json")
+    }
+
+    points = []
+    for part in train_tasks.keys():
+        train = train_tasks[part]
+        infer = infer_tasks[part]
+        points.append(
+            {
+                "path": path,
+                "part": part,
+                "train_task": train.task_id,
+                "infer_task": infer.task_id,
+                "train_time": train.duration,
+                "train_cpu": train.cpu_time,
+                "train_power": train.chassis_power,
+                "infer_time": infer.duration,
+                "infer_cpu": infer.cpu_time,
+                "infer_power": infer.chassis_power,
+            }
+        )
+
+    return pd.DataFrame.from_records(points).set_index(["path", "part"])
+
+
+def _read_tasks(path: Path) -> Generator[CodexTask, None, None]:
+    with path.open("r") as tf:
+        for line in tf:
+            yield CodexTask.model_validate_json(line)
