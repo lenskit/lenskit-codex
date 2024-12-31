8d93525049f1afdb09b98f7820947d41a84a6b5e
Michael Ekstrand <mdekstrand@drexel.edu>
2024-12-31 18:42:03 -0500

    remove unused sql

--- movielens/ml-run-metrics.sql
+++
@@ -1,8 +0,0 @@
-DROP VIEW IF EXISTS variants;
-CREATE VIEW variants AS
-SELECT
-    fileno,
-    regexp_extract(filename, '^runs/(\w+)-[\w-]+/', 1) AS split,
-    regexp_extract(filename, '^runs/\w+-[\w-]+/([a-zA-Z0-9-]+)\.duckdb', 1) AS model,
-    regexp_extract(filename, '^runs/\w+-([\w-]+)/', 1) AS config,
-FROM run_files;
