c200ad6497ae021ac1488bec8d26dec1081afc29
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-17 14:04:33 -0400

    add support for listing documents

--- tasks.py
+++ tasks.py
@@ -1,3 +1,9 @@
+import json
+import re
+from glob import glob
+from pathlib import Path
+
+import yaml
 from invoke.context import Context
 from invoke.tasks import task
 
@@ -20,3 +26,25 @@ def fetch_web_assets(c: Context):
     "Fetch the web assets."
     c.run("dvc pull --no-run-cache -r public dvc.yaml")
     c.run("dvc pull --no-run-cache -r public -R images")
+
+
+@task
+def list_documents(c: Context):
+    "List documents with their metadata."
+    docs = glob("**/*.qmd", recursive=True)
+    print("collecting", len(docs), "documents")
+    docs = {name: _front_matter(name) for name in docs}
+
+    with open("documents.json", "wt") as jsf:
+        json.dump(docs, jsf, indent=2)
+        print(file=jsf)
+
+
+def _front_matter(path):
+    path = Path(path)
+    text = path.read_text("utf8")
+    if m := re.match(r"^---+\s*\n(.*?)\n---+", text, re.DOTALL):
+        print("found metadata in", path)
+        return yaml.safe_load(m.group(1))
+    else:
+        return {}
