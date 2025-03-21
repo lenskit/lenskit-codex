28c10e7a98e8877c26f3024ab1edfeb596e8d218
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-21 12:21:31 -0400

    add model import capabilities

---
+++ src/codex/models/__init__.py
@@ -0,0 +1,102 @@
+from collections.abc import Iterable
+from importlib import import_module
+from pathlib import Path
+from types import ModuleType
+from typing import Any
+
+from lenskit.logging import get_logger
+from lenskit.pipeline import Component
+from pydantic import TypeAdapter
+
+_log = get_logger(__name__)
+model_dir = Path(__file__).parent
+
+
+class ModelDef:
+    """
+    Encapsulation of a Python module defining a model for search.
+    """
+
+    mod_name: str
+    "The name of the model's Python module."
+
+    module: ModuleType
+
+    def __init__(self, name, module):
+        self.mod_name = name
+        self.module = module
+
+    @property
+    def name(self) -> str:
+        return getattr(self.module, "NAME", self.mod_name)
+
+    @property
+    def scorer_class(self) -> type[Component]:
+        return self.module.SCORER
+
+    @property
+    def config_class(self):
+        return self.scorer_class.config_class()
+
+    @property
+    def default_config(self):
+        return self.module.DEFAULT_CONFIG
+
+    @property
+    def static_config(self):
+        return getattr(self.module, "STATIC_CONFIG", {})
+
+    @property
+    def is_predictor(self) -> bool:
+        return getattr(self.module, "PREDICTOR", False)
+
+    @property
+    def search_space(self) -> dict[str, Any]:
+        return getattr(self.module, "SEARCH_SPACE", {})
+
+    def instantiate(self, params: dict[str, Any] | None = None):
+        """
+        Instantiate a model with  specified parameters.
+        """
+        log = _log.bind(name=self.name, module=self.mod_name)
+
+        static = self.static_config
+        if not isinstance(static, dict):
+            static = TypeAdapter(self.config_class).dump_python(static)
+        if static:
+            log.debug("found static configuration", config=static)
+
+        config = static.copy()
+        if params is not None:
+            log.debug("applying configuration", config=params)
+            config.update(params)
+        else:
+            config = self.default_config
+            log.debug("using default configuration", config=config)
+
+        mod_cls = self.scorer_class
+        log.debug("instantiating model", config=config, component=mod_cls)
+
+        model = mod_cls(config)
+        log.info("instantiated model", model=model)
+        return model
+
+
+def discover_models() -> Iterable[str]:
+    """
+    Discover the available model configuration names.
+    """
+    for mf in model_dir.glob("*.py"):
+        if not mf.name.startswith("_"):
+            yield mf.stem
+
+
+def load_model(name: str) -> ModelDef:
+    """
+    Load a model's definition.
+    """
+    fname = name.replace("-", "_").lower()
+
+    _log.info("importing model module", module=name)
+    module = import_module(f"codex.models.{fname}")
+    return ModelDef(fname, module)
---
+++ src/codex/models/bias.py
@@ -0,0 +1,12 @@
+import ray.tune as rt
+from lenskit.basic.bias import BiasConfig, BiasScorer
+
+NAME = "Bias"
+PREDICTOR = True
+SCORER = BiasScorer
+DEFAULT_CONFIG = BiasConfig(damping=50)
+
+SEARCH_SPACE = {
+    "damping.user": rt.loguniform(1e-12, 100),
+    "damping.item": rt.loguniform(1e-12, 100),
+}
