c34b11a5064c2540519886f41d3e673f369123e8
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-22 20:37:09 -0400

    add UKNN

---
+++ src/codex/models/uknn_implicit.py
@@ -0,0 +1,15 @@
+import ray.tune as rt
+from lenskit.knn import UserKNNConfig, UserKNNScorer
+
+PREDICTOR = False
+SCORER = UserKNNScorer
+DEFAULT_CONFIG = UserKNNConfig(max_nbrs=10, min_nbrs=2, feedback="implicit")
+STATIC_CONFIG = {"feedback": "implicit"}
+
+SEARCH_SPACE = {
+    "max_nbrs": rt.randint(2, 50),
+    "min_nbrs": rt.randint(1, 5),
+    "min_sim": rt.loguniform(1e-6, 0.1),
+}
+
+TUNE_CPUS = "2"
