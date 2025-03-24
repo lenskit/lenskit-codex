ee0f111d64f5711f3df4efe16481cc795a050070
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-23 22:53:00 -0400

    start working on model adaptation

--- src/codex/models/iknn_explicit.py
+++ src/codex/models/iknn_explicit.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ray.tune as rt
 from lenskit.knn import ItemKNNConfig, ItemKNNScorer
 
@@ -14,3 +15,19 @@ SEARCH_SPACE = {
 
 TUNE_CPUS = "all"
 OPTIONS = {"search_points": 60}
+
+SEARCH_BASE_CONFIG = ItemKNNConfig(
+    max_nbrs=20, min_nbrs=2, save_nbrs=10000, min_sim=1.0e-6, feedback="explicit"
+)
+
+
+def search_adapt(model: ItemKNNScorer, config: ItemKNNConfig):
+    shrunk = ItemKNNScorer(config)
+    matrix = model.sim_matrix_.copy()
+    matrix.data[matrix.data < config.min_sim] = 0.0
+    matrix.eliminate_zeros()
+    shrunk.items_ = model.items_
+    shrunk.users_ = model.users_
+    shrunk.item_means_ = model.item_means_
+    shrunk.item_counts_ = np.diff(matrix.indices)
+    shrunk.sim_matrix_ = matrix
