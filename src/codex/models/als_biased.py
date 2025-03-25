d983573a1ed2ccd6e4f2218a58e6be07175555e6
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-25 15:58:57 -0400

    add biased ALS

---
+++ src/codex/models/als_biased.py
@@ -0,0 +1,20 @@
+import ray.tune as rt
+from lenskit.als import BiasedMFConfig, BiasedMFScorer
+
+PREDICTOR = True
+SCORER = BiasedMFScorer
+DEFAULT_CONFIG = BiasedMFConfig()
+
+SEARCH_SPACE = {
+    "embedding_size": rt.lograndint(4, 512, base=2),
+    "regularization": {
+        "user": rt.loguniform(1e-5, 1),
+        "item": rt.loguniform(1e-5, 1),
+    },
+    "damping": {
+        "user": rt.loguniform(1e-12, 100),
+        "item": rt.loguniform(1e-12, 100),
+    },
+}
+
+OPTIONS = {"max_epochs": 30}
