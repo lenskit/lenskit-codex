4bfa2d3535edf5fbbdfe01e5a15bacbdbee8c670
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-26 13:10:49 -0400

    add ALS implicit

---
+++ src/codex/models/als_implicit.py
@@ -0,0 +1,21 @@
+import ray.tune as rt
+from lenskit.als import ImplicitMFConfig, ImplicitMFScorer
+
+PREDICTOR = True
+SCORER = ImplicitMFScorer
+DEFAULT_CONFIG = ImplicitMFConfig()
+
+SEARCH_SPACE = {
+    "embedding_size": rt.lograndint(4, 512, base=2),
+    "regularization": {
+        "user": rt.loguniform(1e-5, 1),
+        "item": rt.loguniform(1e-5, 1),
+    },
+    "damping": {
+        "user": rt.loguniform(1e-12, 100),
+        "item": rt.loguniform(1e-12, 100),
+    },
+    "weight": rt.uniform(5, 100),
+}
+
+OPTIONS = {"max_epochs": 30}
