d7f9a6cc359a7ff55d2f01c9f4450fa7710216d5
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-26 17:25:27 -0400

    add flexmf

---
+++ src/codex/models/flexmf_bpr.py
@@ -0,0 +1,22 @@
+import ray.tune as rt
+import torch
+from lenskit.flexmf import FlexMFImplicitConfig, FlexMFImplicitScorer
+
+PREDICTOR = False
+SCORER = FlexMFImplicitScorer
+DEFAULT_CONFIG = FlexMFImplicitConfig(loss="pairwise")
+STATIC_CONFIG = {"loss": "pairwise"}
+
+SEARCH_SPACE = {
+    "embedding_size": rt.lograndint(4, 512, base=2),
+    "regularization": rt.loguniform(1e-5, 1),
+    "learning_rate": rt.loguniform(1e-5, 1e-1),
+    "reg_method": rt.choice(["L2", "AdamW"]),
+    "negative_count": rt.randint(1, 5),
+    "positive_weight": rt.uniform(1, 10),
+}
+
+OPTIONS = {"max_epochs": 50}
+# we can train on CPU, it's just slower
+if torch.cuda.is_available():
+    TUNE_GPUS = 0.25
