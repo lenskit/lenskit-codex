bf493a70aa3b4d0a2f233e91ee96d1d27dee85de
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-26 15:47:16 -0400

    add explicit-feedback FlexMF

---
+++ src/codex/models/flexmf_explicit.py
@@ -0,0 +1,15 @@
+import ray.tune as rt
+from lenskit.flexmf import FlexMFExplicitConfig, FlexMFExplicitScorer
+
+PREDICTOR = True
+SCORER = FlexMFExplicitScorer
+DEFAULT_CONFIG = FlexMFExplicitConfig()
+
+SEARCH_SPACE = {
+    "embedding_size": rt.lograndint(4, 512, base=2),
+    "regularization": rt.loguniform(1e-5, 1),
+    "learning_rate": rt.loguniform(1e-5, 1e-1),
+    "reg_method": rt.choice(["L2", "AdamW"]),
+}
+
+OPTIONS = {}
