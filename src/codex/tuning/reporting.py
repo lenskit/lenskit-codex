1d1b0c3fcf8b540b34c7f30559fdf04902ef508a
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-27 13:55:04 -0400

    simplify an if

--- src/codex/tuning/reporting.py
+++ src/codex/tuning/reporting.py
@@ -74,26 +74,24 @@ class ProgressReport(ray.tune.ProgressReporter):
                     and "training_iteration" in trial.last_result
                 ):
                     tp = self._task_bars.get(trial.trial_id, None)
-                    if trial.last_result and trial.last_result.get("training_iteration", None):
-                        epoch = trial.last_result["training_iteration"]
-                        t_total = trial.last_result.get("max_epochs", None)
+                    epoch = trial.last_result["training_iteration"]
+                    t_total = trial.last_result.get("max_epochs", None)
+                    if tp is None:
+                        bar = item_progress(
+                            "Trial " + trial.trial_id,
+                            total=t_total,
+                            fields={self.metric: ".3f"},
+                        )
+                        tp = TrialProgress(bar, 0)
+                        self._task_bars[trial.trial_id] = tp
 
-                        if tp is None:
-                            bar = item_progress(
-                                "Trial " + trial.trial_id,
-                                total=t_total,
-                                fields={self.metric: ".3f"},
-                            )
-                            tp = TrialProgress(bar, 0)
-                            self._task_bars[trial.trial_id] = tp
-
-                        if epoch > tp.count:
-                            tp.bar.update(
-                                epoch - tp.count,
-                                total=t_total,
-                                **{self.metric: trial.last_result[self.metric]},
-                            )
-                            tp.count = epoch
+                    if epoch > tp.count:
+                        tp.bar.update(
+                            epoch - tp.count,
+                            total=t_total,
+                            **{self.metric: trial.last_result[self.metric]},
+                        )
+                        tp.count = epoch
 
             extra = {self.metric: self.best_metric or np.nan}
             self._bar.update(n_new, total=total, **extra)
