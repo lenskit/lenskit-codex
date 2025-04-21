eda11c1655bd9e990879e678ff53b3a6b921e2ab
Michael Ekstrand <mdekstrand@drexel.edu>
2025-04-21 17:41:07 -0400

    FlexMF WARP version

---
+++ src/codex/models/flexmf_warp.py
@@ -0,0 +1,22 @@
+import ray.tune as rt
+import torch
+from lenskit.flexmf import FlexMFImplicitConfig, FlexMFImplicitScorer
+
+PREDICTOR = False
+SCORER = FlexMFImplicitScorer
+DEFAULT_CONFIG = FlexMFImplicitConfig(loss="warp")
+STATIC_CONFIG = {"loss": "warp"}
+
+SEARCH_SPACE = {
+    "embedding_size": rt.lograndint(4, 512, base=2),
+    "regularization": rt.loguniform(1e-4, 10),
+    "learning_rate": rt.loguniform(1e-3, 1e-1),
+    "reg_method": rt.choice(["L2", "AdamW"]),
+    "negative_count": rt.randint(1, 5),
+    "item_bias": rt.choice([True, False]),
+}
+
+OPTIONS = {"max_epochs": 50}
+# we can train on CPU, it's just slower
+if torch.cuda.is_available():
+    TUNE_GPUS = 0.2
