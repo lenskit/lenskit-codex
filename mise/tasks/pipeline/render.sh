7024da0d6a6b5dce255a6a9ddc54315254ff332b
Michael Ekstrand <mdekstrand@drexel.edu>
2026-06-23 14:04:28 -0400

    pipeline rendering integrated

---
+++ mise/tasks/pipeline/render.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+#MISE description="Re-render the DVC pipelines."
+#USAGE flag "-v --verbose" help="Enable verbose logging."
+
+setopt -eo pipefail
+
+declare -a mk_args=()
+
+if [[ $usage_verbose ]]; then
+    mk_args+=(--verbose)
+fi
+
+for file in **/pipeline.tcl; do
+    dir="$(dirname "$file")"
+    ./scripts/mkpipeline.tcl "${mk_args[@]}" -o "${dir}/dvc.yaml" "$file"
+done
