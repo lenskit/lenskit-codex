2e95fef7743e632004039522ec12b6228da2c40e
Michael Ekstrand <mdekstrand@drexel.edu>
2026-07-02 13:47:13 -0400

    fix some piepline

--- movielens/_pipeline/searches.tcl
+++ movielens/_pipeline/searches.tcl
@@ -22,7 +22,7 @@ if {[info exists movielens(search-points)]} {
 }
 
 foreach mod [model list -enabled $movielens(name)] {
-    set out_dir sweeps/$mod/optuna/$mod
+    set out_dir sweeps/$movielens(split)/optuna/$mod
     stage "sweep-$mod-$movielens(split)-optuna" {
         cmd lenskit codex tune {*}$search_args $mod $out_dir
         dep {*}$search_deps
