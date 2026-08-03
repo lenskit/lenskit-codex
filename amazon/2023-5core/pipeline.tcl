f16972546827f7b0af80f5d5132876091fd9a593
hkn28 <hkn28@drexel.edu>
2026-08-03 12:27:07 -0400

    Office Products runs for Amazon

--- amazon/2023-5core/pipeline.tcl
+++ amazon/2023-5core/pipeline.tcl
@@ -21,7 +21,7 @@ azcat -no-tune IndSci Industrial_and_Scientific
 # Kindle: Kindle_Store
 # MovTV: Movies_and_TV
 azcat -no-tune MusInst Musical_Instruments
-# Office: Office_Products
+azcat -no-tune Office Office_Products
 # PLG: Patio_Lawn_and_Garden
 azcat -no-tune Pet Pet_Supplies
 azcat -no-tune Software Software
